3611ffa9324b766028ee3d57b91363e579b18d18
Ricky <ricky.liang@sjsu.edu>
2025-02-14 00:26:05 -0800

    s

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = {
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  rules: {
+    "react/no-unescaped-entities": "off",
+  },
+};
 
 export default eslintConfig;
